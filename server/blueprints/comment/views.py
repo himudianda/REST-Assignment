0a1c9942e6a3020f17a77518feec95c7f5f37f29
Harshit Imudianda <harshit.imudianda@gmail.com>
2015-11-02 00:26:41 -0800

    More work on CRUD APIs for Comemnt

--- server/blueprints/comment/views.py
+++ server/blueprints/comment/views.py
@@ -36,19 +36,18 @@ class CommentListAPI(Resource):
     # curl -i -X GET http://localhost:8000/comments
     def get(self):
         comments = Comment.query.all()
-        return {'comments': [ _c.serialize for _c in comments ]}, 200
+        if comments:
+            return {'comments': [ _c.serialize for _c in comments ]}, 200
+        else:
+            abort(404)
 
 
     # curl -i -X POST -H "Content-Type: application/json" -d '{"topic":"health", "text": "A glass of Red wine isnt that bad"}' http://localhost:8000/comments
     def post(self):
         args = self.reqparse.parse_args()
-        comment = {
-            'id': comments[-1]['id'] + 1,
-            'topic': args['topic'],
-            'text': args['text']
-        }
-        comments.append(comment)
-        return {'comment': comment}, 201
+        comment = Comment(topic=args['topic'], text=args['text'])
+        comment.save()
+        return {'comment': comment.serialize}, 201
 
 
 class CommentAPI(Resource):
@@ -61,30 +60,33 @@ class CommentAPI(Resource):
 
     # curl -i -X GET http://localhost:8000/comment/1
     def get(self, id):
-        comment = [comment for comment in comments if comment['id'] == id]
-        if len(comment) == 0:
+        comment = Comment.query.get(id)
+        if comment:
+            return {'comment': comment.serialize}, 200
+        else:
             abort(404)
-        return {'comment': comment}
 
     # curl -i -X PUT -H "Content-Type: application/json" -d '{"topic":"entertainment", "text": "Steve jobs movie opened with a 2 star rating."}' http://localhost:8000/comment/4
     def put(self, id):
-        comment = [comment for comment in comments if comment['id'] == id]
-        if len(comment) == 0:
+        comment = Comment.query.get(id)
+        if comment:
+            args = self.reqparse.parse_args()
+            comment.topic = args['topic']
+            comment.text = args['text']
+            comment.save()
+            return {'comment': comment.serialize}, 200
+        else:
             abort(404)
-        comment = comment[0]
-        args = self.reqparse.parse_args()
-        for k, v in args.items():
-            if v is not None:
-                comment[k] = v
-        return {'comment': comment}
+
 
     # curl -i -X DELETE http://localhost:8000/comment/4
     def delete(self, id):
-        comment = [comment for comment in comments if comment['id'] == id]
-        if len(comment) == 0:
-            abort(404)
-        comments.remove(comment[0])
-        return {'result': True}
+        comment = Comment.query.get(id)
+        if comment:
+            comment.delete()
+            return {'result': True}, 204
+        else:
+            return {'result': False}, 404
 
 
 api.add_resource(CommentListAPI, '/comments')
